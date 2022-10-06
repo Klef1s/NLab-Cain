bdcfb5d0bf0a0f99d00f08a41877a2606f26d481
Klef1s God <apetr101@mail.ru>
2022-10-06 21:56:08 +0700

    Добавление базы данных

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NLab_Cain.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=DataBase.db");
+        }
+    }
+}
