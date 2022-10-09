9c2b0acae0428be80139f18231bfd7e05aa73c58
Вячеслав <slnest0725@gmail.com>
2022-10-09 20:24:11 +0700

    Добавлена страница Восстановления пароля(ввод нового пароля)

---
+++ Pages/PasswordRecoveryNewPasswordPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NLab_Cain.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PasswordRecoveryNewPasswordPage.xaml
+    /// </summary>
+    public partial class PasswordRecoveryNewPasswordPage : Page
+    {
+        public PasswordRecoveryNewPasswordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
